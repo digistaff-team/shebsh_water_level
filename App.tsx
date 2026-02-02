26086e70620821c56b3d188b036792acd9715c22
digistaff-team <digistaff.team@gmail.com>
2026-02-02 23:09:31 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -105,7 +105,7 @@ const App: React.FC = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-water-600" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <path d="M12 2.69l5.66 5.66a8 8 0 1 1-11.31 0z" />
             </svg>
-            <h1 className="text-xl font-bold text-slate-800">Уровень реки Шебш</h1>
+            <h1 className="text-xl font-bold text-slate-800">р.Шебш</h1>
           </div>
           
           <button 
@@ -130,7 +130,7 @@ const App: React.FC = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                 </svg>
-                Обновить сейчас
+                Обновить
               </>
             )}
           </button>
